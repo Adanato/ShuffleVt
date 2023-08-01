44ed1b4d6f16f462fa6424a32b53273d4dc70284
Adam Nguyen <Adanatonw@gmail.com>
2023-07-31 22:41:11 -0400

    add videos

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,37 @@
 import "./Home.css";
 
 function Home() {
-  return <main className="home-page-container"></main>;
+  return (
+    <main className="home-page-container">
+      <iframe
+        width="320"
+        height="180"
+        src="https://www.youtube.com/embed/o6gHa1rTQfE"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        width="320"
+        height="180"
+        src="https://www.youtube.com/embed/a4MSqakgyfY"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+      <iframe
+        width="320"
+        height="180"
+        src="https://www.youtube.com/embed/ZzsYms-WkBs"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      ></iframe>
+    </main>
+  );
 }
 
 export default Home;
